579ec478e25beaaa6fa017d0f316889d79dd45c9
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2021-11-16 11:28:34 +0100

    hra co funguje

--- cyklushra/Program.cs
+++ cyklushra/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 
 namespace cyklushra
 {
@@ -6,7 +6,49 @@ namespace cyklushra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Random random = new Random();
+
+            int vyber1;
+            Console.Write("Vyber počáteční číslo");
+            vyber1 = int.Parse(Console.ReadLine());
+
+            int vyber2;
+            Console.Write("Vyber konečné číslo číslo");
+            vyber2 = int.Parse(Console.ReadLine());
+
+            int Nahcislo = random.Next(vyber1, vyber2);
+            int Tipcislo;
+            bool neuhodl = true;
+
+
+
+
+
+                Console.WriteLine("Hádání čísel");
+                Console.WriteLine("----------------------");
+                Console.WriteLine($"Myslím si číslo mezi {vyber1} a {vyber2}");
+
+            while (neuhodl)
+            {
+                Console.WriteLine("Tipni si.");
+                Tipcislo = int.Parse(Console.ReadLine());
+
+                if (Tipcislo > Nahcislo)
+                {
+                    Console.WriteLine("To je moc");
+                }
+                else if (Tipcislo < Nahcislo)
+                {
+                    Console.WriteLine("To je málo");
+                }
+                else
+                {
+                    Console.WriteLine("Trefa!");
+                    neuhodl = false;
+                }
+            }
+           
+        }  
+        
     }
 }
